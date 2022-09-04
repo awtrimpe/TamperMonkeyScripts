c15f18d1af3e77e57d79cff04e2ec6d1d52da994
Alex Trimpe <43074742+awtrimpe@users.noreply.github.com>
2022-09-04 16:22:12 -0400

    Update ZillowPPSF.js

--- scripts/ZillowPPSF.js
+++ scripts/ZillowPPSF.js
@@ -1,22 +1,35 @@
+// ==UserScript==
+// @name         Zillow PPSF
+// @namespace    http://zillow.com/
+// @version      0.1
+// @description  Improving the Zillow UI
+// @author       atrimpe
+// @match        *.zillow.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zillow.com
+// @grant        none
+// @run-at        document-end
+// ==/UserScript==
+
 (() => {
     "use strict";
     document.addEventListener("mouseover", () => {
-        console.log("ALEX!");
         let cards = document.getElementsByClassName("property-card-data");
+        if (cards.length < 1) {
+            console.log('Getting other type');
+            cards = document.getElementsByClassName("list-card-info");
+        }
         for (let card of cards) {
             let price;
             let sqFootage;
             card.childNodes.forEach((node) => {
                 if (node.innerText.includes("$")) {
                     const moneyRegex = /[0-9,]+/g;
-                    console.log(node.innerText.match(moneyRegex)[0]);
                     price = Number(
                         node.innerText.match(moneyRegex)[0].replaceAll(",", "")
                     );
                 }
                 const regex = /((?<=ba)(.*)(?= sqft))/g;
                 if (node.innerText.includes("sqft")) {
-                    console.log(node.innerText.match(regex)[0]);
                     sqFootage = Number(
                         node.innerText.match(regex)[0].replaceAll(",", "")
                     );
